5f168a0f23d83a9ff0cce4198e710a43a49a7682
Dipto1971 <dipto.haq771@gmail.com>
2024-03-10 20:15:04 +0600

    Solved Recursion related problem

---
+++ Problem_Solving/02_MergeTwoSortedLL.cpp
@@ -0,0 +1,52 @@
+// LeetCode: 21. Merge Two Sorted Lists
+// Approach: Recursive approach to merge two sorted linked lists
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (!l1) return l2;
+        if (!l2) return l1;
+        
+        if (l1->val < l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
+    }
+};
+
+// Utility function to print the linked list
+void printList(ListNode* head) {
+    while (head) {
+        std::cout << head->val << " ";
+        head = head->next;
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    ListNode* list1 = new ListNode(1);
+    list1->next = new ListNode(2);
+    list1->next->next = new ListNode(4);
+
+    ListNode* list2 = new ListNode(1);
+    list2->next = new ListNode(3);
+    list2->next->next = new ListNode(4);
+
+    Solution solution;
+    ListNode* mergedList = solution.mergeTwoLists(list1, list2);
+
+    std::cout << "Merged List: ";
+    printList(mergedList);
+
+    return 0;
+}
