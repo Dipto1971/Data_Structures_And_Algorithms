a7d8522febc212e17fad29200bd77918e666ef09
Dipto1971 <dipto.haq771@gmail.com>
2024-05-13 09:47:12 +0600

    Revised Sum of Subsets Dynamic Solution

--- Algorithms/07_DynamicProgramming/04_Sum_of_subsets_dp.cpp
+++ Algorithms/07_DynamicProgramming/04_Sum_of_subsets_dp.cpp
@@ -13,10 +13,12 @@ int countSubsets(int arr[], int n, int sum)
 
     for (int i = 1; i <= n; i++){
         for (int j = 1; j <= sum; j++){
-            if (arr[i - 1] <= j)
-                dp[i][j] = dp[i - 1][j] + dp[i - 1][j - arr[i - 1]];
-            else
-                dp[i][j] = dp[i - 1][j];
+            if(j < arr[i-1]){
+                dp[i][j] = dp[i-1][j];
+            }
+            else{
+                dp[i][j] = dp[i-1][j] + dp[i-1][j-arr[i-1]];
+            }
         }
     }
     return dp[n][sum];
