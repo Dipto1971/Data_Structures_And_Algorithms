561028a828c058cb83d5bd22c19789f25063b864
dipto1971 <dipto.haq771@gmail.com>
2024-06-03 23:12:10 +0600

    .

--- Algorithms/08_ShortestPath/03_BFS.cpp
+++ Algorithms/08_ShortestPath/03_BFS.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #define INF 1e9
 
 void BFS(vector<int> adj[], int V, int src, bool visited[]){
+    // BFS of a connected graph
     queue<int> q;
     q.push(src);
     visited[src] = true;
@@ -23,6 +24,7 @@ void BFS(vector<int> adj[], int V, int src, bool visited[]){
 }
 
 void BFSDis (vector<int> adj[], int V){
+    // BFS of a disconnected graph
     bool visited[V];
     for(int i = 0; i < V; i++){
         visited[i] = false;
@@ -36,19 +38,37 @@ void BFSDis (vector<int> adj[], int V){
 }
 
 int main(){
-    int V = 5;
+    // For disconnected graph
+    int V = 7;
     vector<int> adj[V];
-
     adj[0].push_back(1);
-    adj[0].push_back(2);
+    adj[1].push_back(0);
     adj[1].push_back(2);
-    adj[2].push_back(0);
-    adj[2].push_back(3);
-    adj[3].push_back(3);
-    adj[4].push_back(4);
+    adj[2].push_back(1);
+    adj[3].push_back(4);
+    adj[4].push_back(3);
+    adj[5].push_back(6);
+    adj[6].push_back(5);
 
     cout << "Following is Breadth First Traversal: " << endl;
     BFSDis(adj, V);
+
+    // For connected graph
+    int V = 4;
+    vector<int> adj[V];
+    bool visited[V];
+
+    adj[0].push_back(1);
+    adj[0].push_back(2);
+    adj[1].push_back(0);
+    adj[1].push_back(2);
+    adj[2].push_back(0);
+    adj[2].push_back(1);
+    adj[2].push_back(3);
+    adj[3].push_back(2);
+
+    cout << "Following is Breadth First Traversal: " << endl;
+    BFS(adj, V, 0, visited);
 
     return 0;
 }
