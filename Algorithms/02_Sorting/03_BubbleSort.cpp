8bbb8caa67f6f644d52ab53a12d2e18473447003
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2024-11-27 17:38:24 +0600

    Bubble Sort

--- Algorithms/02_Sorting/03_BubbleSort.cpp
+++ Algorithms/02_Sorting/03_BubbleSort.cpp
@@ -14,6 +14,24 @@ void bubbleSort (arr, n){
     }
 }
 
+void optimizedBubbleSort (arr, n){
+    bool swapped;
+    for (int i = 0; i < n-1; i++){
+        swapped = false;
+        for (int j = 0; j < n-i-1; j++){
+            if (arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+                swapped = true;
+            }
+        }
+        if (swapped == false){
+            break;
+        }
+    }
+}
+// Better because 
+// it reduces the number of iterations in the best case
+
 int main (){
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int n = sizeof(arr)/sizeof(arr[0]);
