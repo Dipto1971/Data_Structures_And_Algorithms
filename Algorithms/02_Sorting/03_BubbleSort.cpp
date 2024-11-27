3088014cd5c9603378094abeedb4943b4a658a4f
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2024-11-27 17:30:34 +0600

    .

--- Algorithms/02_Sorting/03_BubbleSort.cpp
+++ Algorithms/02_Sorting/03_BubbleSort.cpp
@@ -1,3 +1,6 @@
+# include <bits/stdc++.h>
+using namespace std;
+
 void bubbleSort (arr, n){
     for (int i = 0; i < n-1; i++){
         for (int j = 0; j < n-i-1; j++){
--- Algorithms/02_Sorting/04_InsertionSort.cpp
+++ Algorithms/02_Sorting/04_InsertionSort.cpp
@@ -1,3 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 void insertionSort (int arr[], int n){
     for (int i = 1; i < n; i++){
         int key = arr[i]; // Element to be compared
@@ -12,4 +15,15 @@ void insertionSort (int arr[], int n){
         }
         arr[j+1] = key;
     }
+}
+
+int main (){
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    insertionSort(arr, n);
+    cout << "Sorted array: \n";
+    for (int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
 }
