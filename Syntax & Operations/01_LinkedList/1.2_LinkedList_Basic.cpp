0db14576cc3d866ae81c0d1f7963714d58e95d26
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2024-12-19 18:56:44 +0600

    Refactor linked list implementation to remove tail pointer and simplify node insertion

--- Syntax & Operations/01_LinkedList/1.2_LinkedList_Basic.cpp
+++ Syntax & Operations/01_LinkedList/1.2_LinkedList_Basic.cpp
@@ -7,7 +7,6 @@ struct Node{
 };
 
 struct Node *head = NULL;
-struct Node *tail = NULL;
 
 void linkedlistTraversal(struct Node *ptr){ //Time Complexity: O(n)
     while(ptr != NULL){
@@ -17,23 +16,28 @@ void linkedlistTraversal(struct Node *ptr){ //Time Complexity: O(n)
 }
 
 int main(){
+    // Linked list implemeneation only using head pointer
+
     struct Node *cur; //initialization
     int n,i;
     printf("How many Nodes you want to create? -> ");
     scanf("%d", &n);
+
     for(i=0; i<n; i++){
         cur = (struct Node*)malloc(sizeof(struct Node)); //For each Node, allocating a memory space
-        scanf("%d", cur -> value); //picking the value from console
+        printf("Enter the value of Node %d -> ", i+1);
+        scanf("%d", &cur -> value); //picking the value from console
         cur -> next = NULL; 
         if(head == NULL){ //for the first Node of Linkelist chain head will be NULL
             head = cur; //starting my chain from that point
-            tail = cur; //this will change continuously
         }
         else {
-            tail -> next = cur; //Everytime changes the direction to the Next Node
-            tail = cur;
+            struct Node *ptr = head;
+            while(ptr -> next != NULL){
+                ptr = ptr -> next;
+            }
+            ptr -> next = cur;
         }
     }
     linkedlistTraversal(head);
-
 }
